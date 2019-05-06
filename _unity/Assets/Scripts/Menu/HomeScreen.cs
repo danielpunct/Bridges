acf8b867339e677d0fd5deeb57fdd787614fd389
Daniel L <liviudaniel.lupascu@gmail.com>
2019-05-06 22:14:01 +0300

    Interface 2h

---
+++ _unity/Assets/Scripts/Menu/HomeScreen.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeScreen : MonoBehaviour
+{
+   public void OnPlayClick()
+   {
+      MenuManager.Instance.ShowGameMenu();
+   }
+}
---
+++ _unity/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,30 @@
+﻿using Gamelogic.Extensions;
+using UnityEngine;
+
+public class MenuManager : Singleton<MenuManager>
+{
+    public GameObject homeScreen;
+    public GameObject gameScreen;
+
+    void Start()
+    {
+        _Reset();
+    }
+
+    public void _Reset()
+    {
+        ShowHomeMenu();
+    }
+
+    public void ShowGameMenu()
+    {
+        homeScreen.SetActive(false);
+        gameScreen.SetActive(true);
+    }
+
+    public void ShowHomeMenu()
+    {
+        homeScreen.SetActive(true);
+        gameScreen.SetActive(false);
+    }
+}
