3c55eef5c3ea2178b7dd11b7281bb8065d970e74
Daniel Lupascu <daniel.lupascu@erepubliklabs.com>
2019-05-23 19:31:03 +0300

    animations

---
+++ _unity/Assets/Scripts/Menu/ButtonAnimator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class ButtonAnimator : MonoBehaviour
+{
+   Transform _tr;
+   Sequence _seq;
+
+   void Awake()
+   {
+      _tr = transform;
+   }
+
+   void OnEnable()
+   {
+      InvokeRepeating("Beat", 3, 3);
+   }
+
+   public void Beat()
+   {
+      _seq?.Kill();
+
+      _seq = DOTween.Sequence()
+         .Insert(0, t: _tr.DOPunchScale(Vector3.one * 0.1f,
+            duration: 1f,vibrato: 4,elasticity: 0.6f));
+   }
+}
---
+++ _unity/Assets/Scripts/Menu/TitleAnimator.cs
@@ -0,0 +1,23 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class TitleAnimator : MonoBehaviour
+{
+    public Transform titleHolder;
+    public Transform ballHolder;
+   public  AnimationCurve ballCurve;
+    Sequence _seq;
+    
+    void OnEnable()
+    {
+        ballHolder.DOLocalMoveY(700,0);
+        
+        
+        _seq?.Kill();
+
+        _seq = DOTween.Sequence()
+            .Insert(0, ballHolder.DOLocalMoveY(0, 0.6f).SetEase(ballCurve))
+            .Insert(0.6f, titleHolder.DOPunchScale(Vector3.one * 0.2f, 1f, 4, 0.3f));
+
+    }
+}
